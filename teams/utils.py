2dd349ad2a2ace784c1b0598892b1fd7958bf01e
matheuscdd <matheuscdd@hotmail.com>
2023-03-30 14:33:36 -0300

    feat: tarefa criar app team finalizada

--- teams/utils.py
+++ teams/utils.py
@@ -6,10 +6,10 @@ def data_processing(team: dict):
     from datetime import datetime
     valid_years = [y for y in range(1930, datetime.now().year, 4)]
     if year < 1930 or year not in valid_years:
-        raise InvalidYearCupError("there was no world cup this year")
+        raise InvalidYearCupError('there was no world cup this year')
     elif titles < 0:
-        raise NegativeTitlesError("titles cannot be negative")
+        raise NegativeTitlesError('titles cannot be negative')
     years_after_first_cup = len(valid_years) - (valid_years.index(year)+1)
     if years_after_first_cup < titles:
-        raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
+        raise ImpossibleTitlesError('impossible to have more titles than disputed cups')
 
